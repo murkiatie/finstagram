980e388b78984391d6b2dde9ac6dc157c06a7ceb
murkiatie <bacdosg@gmail.com>
2022-02-19 22:48:22 +0000

    W6D1: Session Hash, current user, login/logout

--- app/actions.rb
+++ app/actions.rb
@@ -1,52 +1,57 @@
-def humanized_time_ago (time_ago_in_minutes)
-    if time_ago_in_minutes >= 60
-        "#{time_ago_in_minutes / 60} hours ago"
-    else
-        "#{time_ago_in_minutes} minutes ago"
+helpers do
+    def current_user
+      User.find_by(id: session[:user_id])
     end
-end   
+  end
 
 get '/' do
-    @finstagram_post_shark = {
-        username: "sharky_j",
-        avatar_url: "http://naserca.com/images/sharky_j.jpg",
-        photo_url: "http://naserca.com/images/shark.jpg",
-        humanized_time_ago: humanized_time_ago(15),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "sharky_j",
-            text: "Out for the long weekend...too embarrassed to show y'all the beach bod!"
-        }] 
-    }   
+    @finstagram_posts = FinstagramPost.order(created_at: :desc)
+    erb( :index)
+end
 
-    @finstagram_post_whale = {
-        username: "kirk_whalum",
-        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-        photo_url: "http://naserca.com/images/whale.jpg",
-        humanized_time_ago: humanized_time_ago(65),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "kirk_whalum", 
-            text: "#weekendvibes"
-        }]
-    }   
+get '/signup' do
+    @user = User.new
+    erb( :signup)
+end
 
-    @finstagram_post_marlin = {
-        username: "marlin_peppa",
-        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-        photo_url: "http://naserca.com/images/marlin.jpg",
-        humanized_time_ago: humanized_time_ago(190),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "marlin_peppa",
-            text: "lunchtime! ;)"
-        }]
-    }
+post '/signup' do
 
-    @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
-    
-erb(:index)    
+    email      = params[:email]
+    avatar_url = params[:avatar_url]
+    username   = params[:username]
+    password   = params[:password]
+
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+    if @user.save
+        redirect to('/login')
+    else
+        erb(:signup)
+    end
+end
+
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+
+    user = User.find_by(username: username)
+
+    if user && user.password == password
+        session[:user_id] = user.id
+        #"Success! User with id #{session[:user_id]} is logged in!"
+        redirect to ('/')
+    else
+        @error_message = "Login failed."
+        erb(:login)  
+    end
+end 
+
+get '/logout' do
+    session[:user_id] = nil
+    #"Logout successful!"
+    redirect to ('/')
 end
